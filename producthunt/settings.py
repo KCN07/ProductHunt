21e2dd420e7084a3be61e282cddf8e44ac5bb272
KCN007 <www.kaniz98@gmail.com>
2020-07-25 15:30:24 +0600

    fixed something again

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'producthuntdb',
         'USER': 'postgres',
-        'PASSWORD': 'kaniz10742',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
